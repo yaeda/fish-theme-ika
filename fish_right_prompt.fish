ad4ea6116342764a5695b87c83f3af13ffc0b422
Takeshi YAEDA <t.yaeda@gmail.com>
2017-08-18 22:10:33 +0900

    :rocket: Support Festival and Improve tmp file handling :dancers:

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -1,18 +1,15 @@
 function fish_right_prompt
-  set -l json_path '/tmp/fish_theme_ika.json'
+  set -l path_timestamp '/tmp/ika_timestamp.txt'
+  set -l path_schedule '/tmp/ika_schedule.json'
+  set -l path_fes '/tmp/ika_fes.json'
 
-  if not test -e $json_path
-    _update_json_async $json_path
-    return 0
-  end
+  _needs_update $path_timestamp $path_schedule $path_fes
+  and _update_json_async $path_timestamp $path_schedule $path_fes
+  and return 0
 
-  set -l rule_key (_current_rule $json_path)
+  set -l rule_key (_current_rule $path_schedule $path_fes)
   set -l rule_name (_translate $rule_key $theme_ika_lang)
-
-  # TODO: care about fes
-  # TODO: care about downloading state
   if test $status -eq 1
-    _update_json_async $json_path
     return 0
   end
 
@@ -21,25 +18,37 @@ function fish_right_prompt
   set_color normal
 end
 
-function _update_json_async
-  set -l api_endpoint 'https://splatoon2.ink/data/schedules.json'
-  fish -c "curl -s $api_endpoint > $argv" &
+function _needs_update --argument-names path_timestamp path_schedule path_fes
+  test ! -e $path_timestamp    # timestamp file is not existed
+  or test ! -e $path_schedule  # schedule.json is not existed
+  or test ! -s $path_schedule  # schedule.json is empty (caused by network error)
+  or test ! -e $path_fes       # fes.json is not existed
+  or test ! -s $path_fes       # fes.json is empty (caused by network error)
+  or test (math (date "+%s") - (cat $path_timestamp)) -ge (math 60 \* 60 \* 12) # cache 12h
 end
 
-function _current_rule
-  set -l current_time (date "+%s")
-  echo (command cat $argv | jq -r ".gachi[] | select(.start_time < $current_time) | select(.end_time > $current_time) | .rule.key")
+function _update_json_async --argument-names path_timestamp path_schedule path_fes
+  set -l api_endpoint_schedule 'https://splatoon2.ink/data/schedules.json'
+  set -l api_endpoint_fes 'https://splatoon2.ink/data/festivals-na.json'
+  fish -c "curl -s $api_endpoint_schedule > $path_schedule" &
+  fish -c "curl -s $api_endpoint_fes > $path_fes" &
+  command date "+%s" > $path_timestamp
 end
 
-function _translate
-  set -l rule_key $argv[1]
-  set -l lang "JP"
-  if set -q argv[2]
-    set lang $argv[2] 
+function _current_rule --argument-names path_schedule path_fes
+  if test (command cat $path_fes | jq -r ".festivals | length") -ne 0
+    echo 'fes'
+  else 
+    set -l current_time (date "+%s")
+    echo (command cat $path_schedule | jq -r ".gachi[] | select(.start_time < $current_time) | select(.end_time > $current_time) | .rule.key")
   end
+end
 
-  set -l rule_name_list_jp "ガチエリア" "ガチホコ" "ガチヤグラ"
-  set -l rule_name_list_en "Splat Zones" "Rainmaker" "Tower Control"
+function _translate --argument-names rule_key lang
+  set -q argv[2]; or set lang 'JP'
+
+  set -l rule_name_list_jp "フェス" "ガチエリア" "ガチホコ" "ガチヤグラ"
+  set -l rule_name_list_en "Festival" "Splat Zones" "Rainmaker" "Tower Control"
   set -l rule_name_list
   switch $lang
     case "JP"
@@ -51,14 +60,17 @@ function _translate
   end
 
   switch $rule_key
-    case 'splat_zones'
+    case 'fes'
       echo $rule_name_list[1]
       return 0
-    case 'rainmaker'
+    case 'splat_zones'
       echo $rule_name_list[2]
       return 0
-    case 'tower_control'
+    case 'rainmaker'
       echo $rule_name_list[3]
+      return 0
+    case 'tower_control'
+      echo $rule_name_list[4]
       return 0
     case '*'
       return 1
